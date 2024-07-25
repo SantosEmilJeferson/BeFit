973b69872fa5bba679fc6d49ba7a07818d4a1ccc
SantosEmilJeferson <119147087+SantosEmilJeferson@users.noreply.github.com>
2024-07-26 00:07:49 +0800

    Add files via upload

---
+++ view/login/login_view.dart
@@ -0,0 +1,261 @@
+import 'package:fitness/view/main_tab/main_tab_view.dart';
+import 'package:flutter/material.dart';
+import 'package:fitness/common/colo_extension.dart';
+import 'package:fitness/common_widget/round_button.dart';
+import 'package:fitness/common_widget/round_textfield.dart';
+import 'package:fitness/view/login/complete_profile_view.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class ValidationHelper {
+  bool isEmailValid(String email) {
+    // Basic email validation logic, you can adjust as needed
+    return email.isNotEmpty && email.contains('@');
+  }
+
+  bool isPasswordValid(String password) {
+    // Password validation criteria
+    bool isLengthValid = password.length >= 8;
+    bool isNumberValid = password.contains(RegExp(r'\d'));
+    bool isSpecialCharValid =
+        password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+    bool isUpperCaseAndLowerCaseValid = password.contains(RegExp(r'[A-Z]')) &&
+        password.contains(RegExp(r'[a-z]'));
+
+    return isLengthValid &&
+        isNumberValid &&
+        isSpecialCharValid &&
+        isUpperCaseAndLowerCaseValid;
+  }
+}
+
+class _LoginViewState extends State<LoginView> {
+  final ValidationHelper _validator = ValidationHelper();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: TColor.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            height: media.height * 0.9,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Hey there,",
+                  style: TextStyle(color: TColor.gray, fontSize: 16),
+                ),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                      color: TColor.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  height: media.width * 0.05,
+                ),
+                RoundTextField(
+                  controller: _emailController,
+                  hitText: "Email",
+                  icon: "assets/img/email.png",
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                RoundTextField(
+                  controller: _passwordController,
+                  hitText: "Password",
+                  icon: "assets/img/lock.png",
+                  obscureText: true,
+                  rigtIcon: TextButton(
+                    onPressed: () {},
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 20,
+                      height: 20,
+                      child: Image.asset(
+                        "assets/img/show_password.png",
+                        width: 20,
+                        height: 20,
+                        fit: BoxFit.contain,
+                        color: TColor.gray,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Forgot your password?",
+                      style: TextStyle(
+                          color: TColor.gray,
+                          fontSize: 10,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                RoundButton(
+                  title: "Login",
+                  onPressed: () {
+                    String email = _emailController.text.trim();
+                    String password = _passwordController.text;
+
+                    if (_validator.isEmailValid(email) &&
+                        _validator.isPasswordValid(password)) {
+                      // Navigate to MainTabView upon successful login
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainTabView()),
+                      );
+                    } else {
+                      // Handle invalid credentials (optional: show error message)
+                      // For simplicity, you can add a Snackbar or dialog to inform the user
+                      // that credentials are invalid.
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Invalid Credentials'),
+                          content:
+                              Text('Please check your email and password.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('OK'),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: TColor.gray.withOpacity(0.5),
+                      ),
+                    ),
+                    Text(
+                      "  Or  ",
+                      style: TextStyle(color: TColor.black, fontSize: 12),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: TColor.gray.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: TColor.white,
+                          border: Border.all(
+                            width: 1,
+                            color: TColor.gray.withOpacity(0.4),
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Image.asset(
+                          "assets/img/google.png",
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: media.width * 0.04,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: TColor.white,
+                          border: Border.all(
+                            width: 1,
+                            color: TColor.gray.withOpacity(0.4),
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Image.asset(
+                          "assets/img/facebook.png",
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Don’t have an account yet? ",
+                        style: TextStyle(
+                          color: TColor.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        "Register",
+                        style: TextStyle(
+                            color: TColor.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
